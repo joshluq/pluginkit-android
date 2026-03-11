def8e1663a13224f86da5e461736b2b144942c0b
Josh Luque <josh.luque@gmail.com>
2026-03-11 11:27:01 +0100

    Update Hilt version to 2.59.2 and Jackson version to 2.21.1 in Gradle catalog

--- build-logic/build.gradle.kts
+++ build-logic/build.gradle.kts
@@ -6,13 +6,13 @@ plugins {
 group = "es.joshluq.kit.pluginkit"
 version = "1.2.0-SNAPSHOT"
 
-val agpVersion = "9.0.1"
+val agpVersion = "9.1.0"
 val kotlinVersion = "2.3.10"
 val detektVersion = "1.23.8"
 val sonarVersion = "7.1.0.6387"
 val koverVersion = "0.9.7"
 val hiltVersion = "2.59.2"
-val spotlessVersion = "8.2.1"
+val spotlessVersion = "8.3.0"
 val kspVersion = "2.3.4"
 
 repositories {
