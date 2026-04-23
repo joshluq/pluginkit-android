bfd0e9024c40cb0804591347ec0e145706bbc848
Josh Luque <josh.luque@gmail.com>
2026-04-23 12:01:20 +0200

    Update Gradle to 9.4.1, AGP to 9.2.0, and Kotlin to 2.3.21 in Gradle wrapper, build logic, and catalog

--- build-logic/build.gradle.kts
+++ build-logic/build.gradle.kts
@@ -6,8 +6,8 @@ plugins {
 group = "es.joshluq.kit.pluginkit"
 version = "1.4.0-SNAPSHOT"
 
-val agpVersion = "9.1.1"
-val kotlinVersion = "2.3.20"
+val agpVersion = "9.2.0"
+val kotlinVersion = "2.3.21"
 val detektVersion = "1.23.8"
 val sonarVersion = "7.1.0.6387"
 val koverVersion = "0.9.8"
