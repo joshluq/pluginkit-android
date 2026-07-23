55432335ddd6bff61e3faa347fe0a2c08befb773
Josh Luque <josh.luque@gmail.com>
2026-07-24 00:03:24 +0200

    Update project and plugin versions to 1.7.0-SNAPSHOT and AGP to 9.3.1 in Gradle catalog and build logic

--- build-logic/build.gradle.kts
+++ build-logic/build.gradle.kts
@@ -4,9 +4,9 @@ plugins {
 }
 
 group = "es.joshluq.kit.pluginkit"
-version = "1.6.0"
+version = "1.7.0-SNAPSHOT"
 
-val agpVersion = "9.3.0"
+val agpVersion = "9.3.1"
 val kotlinVersion = "2.4.10"
 val detektVersion = "1.23.8"
 val sonarVersion = "7.1.0.6387"
--- gradle-catalog/build.gradle.kts
+++ gradle-catalog/build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
 }
 
 group = "es.joshluq.kit"
-version = "1.6.0"
+version = "1.7.0-SNAPSHOT"
 
 catalog {
     versionCatalog {
